d80ffd4c46966e2e6bd5d89bfd8557f5d112200c
Gabriel-filhodoneijo <70727346+Gabriel-filhodoneijo@users.noreply.github.com>
2020-10-22 15:10:40 -0300

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,14 @@
+let anoAtual  = 2020;
+
+let nome = prompt("Qual o seu nome?");
+
+let anoDeNascimento = prompt("Qual seu ano de nascimento?");
+
+let z = anoAtual - anoDeNascimento
+
+alert(`Olá, seu nome é ${nome} e sua idade é ${z}`);
+
+
+
+
+
